2cfae3498f99e6b03182ad422c3dd2234d8bdcdd
deveshm8 <60978696+deveshm8@users.noreply.github.com>
2022-06-21 12:33:43 +0530

    Create details.js

---
+++ user tag and map/details.js
@@ -0,0 +1,22 @@
+const details=[{name:"Toni Kroos",
+source:"https://img.asmedia.epimg.net/resizer/NTASDtOD9lJz0We30jbDDFkhgQc=/1952x1098/filters:focal(302x134:312x144)/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/U4CWS2GTRGX32L6GSIJ563Q7KU.jpg",
+club:"Real Madrid",
+country:"Germany"},
+{name:"Karim Benzema",
+source:"https://c.ndtvimg.com/2021-11/ogf9rhs8_karim-benzema-afp_625x300_24_November_21.jpg?im=FaceCrop,algorithm=dnn,width=806,height=605",
+club:"Real Madrid",
+country:"France"},
+{
+  name:"Luka Modric",
+  source:"https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt072cbf85edb0ea9d/62a0544b84241711ab190a1f/modric2.jpg",
+  club:"Real Madrid",
+  country:"Croatia"
+},
+{
+  name:"Robert Lewandowski",
+  source:"https://estaticos-cdn.sport.es/clip/4b1c90da-7d9d-482b-a5b0-b2761b3ddfdc_alta-libre-aspect-ratio_default_0.jpg",
+  club:"Bayern Munich",
+  country:"Poland"
+}];
+
+export default details;
