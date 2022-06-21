d9f0cf04b54d92c98dfb41384803cb980aeeff0b
deveshm8 <60978696+deveshm8@users.noreply.github.com>
2022-06-21 12:50:33 +0530

    Update index.js

--- user tag and map/index.js
+++ user tag and map/index.js
@@ -2,17 +2,25 @@ import React from "react";
 import ReactDom from "react-dom";
 import Details from "./details.js";
 import "./styles.css"
-function Card(Details)
+function Card(props){
+  return <div>
+    <h2>{props.name}</h2>
+    <img src={props.source} width ="250px"/>
+    <p>Club:{props.club}</p>
+    <p>Country:{props.country}</p>
+    </div>
+}
+function insertCard(details)
 {
-  return <Card name={Details.name} 
-          source={Details.source}
-          club={Details.club}
-          country={Details.country}/>
+  return <Card name={details.name} 
+          source={details.source}
+          club={details.club}
+          country={details.country}/>;
   }
 const r=document.getElementById("root");
 ReactDom.render(
   <div>
-    {Details.map(Card)}
+    {Details.map(insertCard)}
 </div>,
 r
 );
